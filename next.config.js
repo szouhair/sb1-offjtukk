ffe675ab5c503dca3b2cae80101eda40f355afe0
szouhair <sofian.zou@hotmail.fr>
2025-06-02 01:26:56 +0200

    Can't resolve 'bufferutil' and 'utf-8-validate'

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+//const nextConfig = {
+//  output: 'export',
+//  eslint: {
+//    ignoreDuringBuilds: true,
+//  },
+//  images: { unoptimized: true },
+//};
+
+//module.exports = nextConfig;
+
+const nodeExternals = require('webpack-node-externals');
+
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [nodeExternals()];
+    }
+
+    return config;
+  },
+};
